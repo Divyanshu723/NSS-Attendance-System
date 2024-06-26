dc822710679c0a8dcd25cd0df308265281380c13
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2024-06-26 17:16:19 +0530

    url change

--- frontend/src/Components/services.js
+++ frontend/src/Components/services.js
@@ -1,2 +1,2 @@
-// export const backend_url = "https://nss-attendance-system-backend.onrender.com";
-export const backend_url = "http://localhost:8000";
+export const backend_url = "https://nss-attendance-system-backend.onrender.com";
+// export const backend_url = "http://localhost:8000";
